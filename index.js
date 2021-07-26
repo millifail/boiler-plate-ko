75dd06d831b88b5b78ec693bf611b1c41e637040
DESKTOP-K1BDLKT\황세훈 <hwangbird99@gmail.com>
2021-07-26 17:33:27 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require('./models/User.js');
+const config = require('./config/key');
+
+// // application / x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// // application / json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://shoon99:tpgns5804!@boilerplate.mf2w5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('mongoDB connected...'))
-.catch(err=>console.log(err))
+.catch((err)=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕')
+  res.send('Hello World! 안녕 gg')
+})
+
+app.post('/register',(req,res)=>{
+  //회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
+
+  //req.body  -> {id:"hello", pw:123}   이런 형식임   , body-parser가 있기에 가능한 것
+  const user= new User(req.body)
+
+
+  //save : mongoDB의 메서드  -> 정보들이 user모델에 저장이 된 것
+  user.save((err,userInfo)=>{
+    if(err) {return res.json({success:false,err}) }
+    //status(200) -> 성공했다는 뜻
+    return res.status(200).json({
+      success:true,
+    })
+  })  
+
 })
 
 
